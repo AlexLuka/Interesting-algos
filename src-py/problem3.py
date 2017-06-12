aefe09ce5297cb7f0e4805b6048ecb8f30799de9
AlexLuka <alexbabyboomer@gmail.com>
2017-06-12 00:43:23 -0500

    Problem 3: white noise image added

--- src-py/problem3.py
+++ src-py/problem3.py
@@ -1,6 +1,9 @@
 # This is not a problem )))
 # It is more like algorithm for generation of a rough surface
-from Tkinter import Frame, Tk, BOTH, X, RIGHT, LEFT
+import time
+import numpy as np
+from PIL import Image, ImageTk
+from Tkinter import Frame, Tk, BOTH, X, Y, RIGHT, LEFT
 from ttk import Button, Entry, Label, Combobox
 
 
@@ -9,14 +12,22 @@ class MainFrame(Frame):
     w_ = 500
     h_ = 400
 
+    pad_l = 10
+
     def __init__(self, parent):
         Frame.__init__(self, parent, background='white')
 
         self.parent_ = parent
 
+        # this is the main frame we are working on
+        self.img_frame = Frame(self, background='navy')
         self.init_ui()
-
         self.centering()
+
+        # broken TV app))
+        # while True:
+        self.init_random_image()
+        # time.sleep(0.05)
 
     def centering(self):
         pos_x = (self.parent_.winfo_screenwidth() - self.w_) / 2
@@ -28,7 +39,7 @@ class MainFrame(Frame):
         self.parent_.title('Rough surface generator')
         self.pack(fill=BOTH, expand=True)
 
-        # top panel
+        # top panel with controls
         frame_top = Frame(self, background='gray75')
         frame_top.pack(fill=X)
 
@@ -42,12 +53,41 @@ class MainFrame(Frame):
         entry2 = Entry(frame_top)
         entry2.pack(side=LEFT, padx=5, expand=True)
 
-        but1 = Button(frame_top, text='RUN')
+        but1 = Button(frame_top, text='RUN', command=self.button_action)
         but1.pack(side=RIGHT, padx=5, pady=5)
 
-        # central panel
-        frame_top = Frame(self, background='white')
-        frame_top.pack(fill=BOTH, expand=True)
+        # central panel. It will have a label with an image. Image may have a random noise state, or
+        # transformed image state
+        self.img_frame.pack(fill=BOTH, expand=True)
+
+        img_label = Label(self.img_frame, background=None)
+        img_label.pack(expand=True, fill=BOTH, padx=5, pady=5)
+
+    def init_random_image(self):
+        """
+            Init white noise image on the main panel
+        """
+        self.update()
+
+        img = ImageTk.PhotoImage(
+            Image.fromarray(
+                np.random.random_integers(0, 255,
+                                          (self.img_frame.winfo_height()-self.pad_l,
+                                           self.img_frame.winfo_width()-self.pad_l)).astype(np.int32)
+            ).convert('I')
+        )
+
+        keys = self.img_frame.children.keys()
+        for key in keys:
+            self.img_frame.children[key].configure(image=img)
+            self.img_frame.children[key].image = img
+
+    def button_action(self):
+        """
+            Update the main panel: if it contains white noise -> transform it
+                                   if it contains transformed image -> remove it and create white noise
+        """
+        self.init_random_image()
 
 
 #
