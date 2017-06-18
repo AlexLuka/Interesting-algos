8b139dabd56f412f73d475ed6e38295aeb387a85
AlexLuka <alexbabyboomer@gmail.com>
2017-06-18 00:04:34 -0500

    Gaussian filter implemented

--- src-py/problem3.py
+++ src-py/problem3.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image, ImageTk
 from Tkinter import Frame, Tk, BOTH, X, Y, RIGHT, LEFT
 from ttk import Button, Entry, Label, Combobox
+from matplotlib import cm
 
 
 class MainFrame(Frame):
@@ -26,6 +27,7 @@ class MainFrame(Frame):
 
         # broken TV app))
         # while True:
+        self.state = 0
         self.init_random_image()
         # time.sleep(0.05)
 
@@ -69,13 +71,59 @@ class MainFrame(Frame):
         """
         self.update()
 
+        # set a colormap
+        c_map = cm.get_cmap('bwr')
+
+        # width and height of the image
+        w_ = self.img_frame.winfo_width()-self.pad_l
+        h_ = self.img_frame.winfo_height()-self.pad_l
+
+        # generate random noise
+        random_map = np.random.random((h_, w_))
+
+        # generate a meshgrid for the filter
+        xv, yv = np.meshgrid(np.linspace(-int(w_ / 2.), int(w_ / 2.), w_),
+                             np.linspace(-int(h_ / 2.), int(h_ / 2.), h_))
+
+        # define correlation length and width
+        clx = 100
+        cly = 50
+
+        # this is a gauss filter
+        filter_gauss = np.exp(-np.power(xv, 2) / clx - np.power(yv, 2) / cly)
+
+        # this is a resulting map
+        random_map = np.fft.ifft2(np.multiply(np.fft.fft2(random_map), np.fft.fft2(filter_gauss)))
+        random_map = np.real(random_map)
+
+        # normalize to [0, 1]
+        random_map -= np.min(random_map)
+        random_map /= np.max(random_map)
+
+        # create PhotoImage object to add on the panel
         img = ImageTk.PhotoImage(
             Image.fromarray(
-                np.random.random_integers(0, 255,
-                                          (self.img_frame.winfo_height()-self.pad_l,
-                                           self.img_frame.winfo_width()-self.pad_l)).astype(np.int32)
-            ).convert('I')
+                # image really likes unsigned ints))
+                np.uint8(
+                    # convert to colormap you like
+                    c_map(
+                        # give a random image to color map with values between 0 and 1
+                        # np.random.random(
+                        #     (self.img_frame.winfo_height()-self.pad_l, self.img_frame.winfo_width()-self.pad_l)
+                        # )
+                        random_map
+                    )*255
+                )
+            )
         )
+
+        # Gray colormap
+        # img = ImageTk.PhotoImage(
+        #     Image.fromarray(np.random.random_integers(0, 255,
+        #                                   (self.img_frame.winfo_height()-self.pad_l,
+        #                                    self.img_frame.winfo_width()-self.pad_l)).astype(np.int8)
+        #                     ).convert('L')
+        # )
 
         keys = self.img_frame.children.keys()
         for key in keys:
@@ -84,8 +132,6 @@ class MainFrame(Frame):
 
     def button_action(self):
         """
-            Update the main panel: if it contains white noise -> transform it
-                                   if it contains transformed image -> remove it and create white noise
         """
         self.init_random_image()
 
